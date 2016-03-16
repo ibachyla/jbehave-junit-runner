c7dd59ba814db0908f76875ad6c3ea8558a169b1
Valery Yatsynovich <Valery_Yatsynovich@epam.com>
2016-03-16 10:59:32 +0300

    Add protected getter for configuredEmbedder

--- src/main/java/de/codecentric/jbehave/junit/monitoring/JUnitReportingRunner.java
+++ src/main/java/de/codecentric/jbehave/junit/monitoring/JUnitReportingRunner.java
@@ -226,4 +226,8 @@ public class JUnitReportingRunner extends BlockJUnit4ClassRunner {
 				name));
 		numberOfTestCases++;
 	}
+
+    protected Embedder getConfiguredEmbedder() {
+        return configuredEmbedder;
+    }
 }
